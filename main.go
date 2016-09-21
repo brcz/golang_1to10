412ad4a2597f9a06113ac9f54500d4637b6271a7
Max Damage <brcz@nm.ru>
2016-09-21 18:25:59 +0300

    initial commit

---
+++ main.go
@@ -0,0 +1,32 @@
+package main
+
+
+type SomeParentResp struct {
+    One  [10]string `json:"one"`
+    Two  [10]int    `json:"two"`
+    Sub1 ElemRes    `json:"sub1"`
+    Sub2 ElemRes    `json:"sub2"`
+}
+
+type ElemRes struct {
+    One  [10]string `json:"one"`
+    Two  [10]int    `json:"two"`
+}
+
+//should be splited to
+
+type SomeParent struct {
+    One string
+    Two int
+    Sub1 Elem
+    Sub2 Elem
+}
+
+type Elem struct {
+    One string
+    Two int
+}
+
+
+func main() {
+}
